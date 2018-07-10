5db0bb1d2e727e42a0e25b3b1410d29cffb5cf1d
Red Medrano <johnredsmedrano@gmail.com>
2018-07-10 10:50:10 +0800

    Revert "Add MySQL support to Web Application"
    This reverts commit 636e878c326987ce5a4ac4beff9752e146c44c63.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'mysite',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
